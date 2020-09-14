f1f10f70e16b9f7b05414d2dc1f02f39c3f3ebf2
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-14 16:17:55 +1000

    tables set up locally

--- backend/database.sql
+++ backend/database.sql
@@ -3,20 +3,19 @@ CREATE DATABASE fourbirds;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE Results(
-    id uuid uuid_generate_v4 (),
-    question_id REFERENCES Questions(id),
+    id uuid DEFAULT uuid_generate_v4 (),
+    question_id SERIAL REFERENCES Questions(id),
     answer int,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Questions(
     id SERIAL PRIMARY KEY,
-    q_text TEXT,
-)
+    q_text TEXT
+);
 
-CREATE TABLE User(
+CREATE TABLE Users(
     username VARCHAR(255) PRIMARY KEY,
     password TEXT,
-    user_id REFERENCES Results(id)
-    
-)
+    user_id uuid REFERENCES Results(id)
+);
