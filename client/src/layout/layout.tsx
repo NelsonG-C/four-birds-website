8699159c33a8193625f33ed0bf906fd4290dfdb3
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-16 14:12:54 +1000

    general page layout add

---
+++ client/src/layout/layout.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from "../logo.svg";
+
+export const Layout = (props: any) => {
+  return (
+    <div>
+      <header>
+        <Navbar className="bg-light" expand="lg">
+          <Navbar.Brand>
+            <img
+              src={logo}
+              alt=""
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Four Birds
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="">About</Nav.Link>
+              <Nav.Link href="">The Birds</Nav.Link>
+              <Nav.Link href="">Data</Nav.Link>
+              <Nav.Link href="">Login</Nav.Link>
+              <Nav.Link href="">Take The Test</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+      <body>
+        <Container>{props.children}</Container>
+      </body>
+      <footer>
+        <Container>
+          <p>Created By Nelson GC</p>
+          <p>Built with React</p>
+        </Container>
+      </footer>
+    </div>
+  );
+};
