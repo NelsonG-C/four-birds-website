f993c823d387de7e8edfaa4adf41522c3127f6b5
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-16 22:09:41 +1000

    the test page add

---
+++ client/src/pages/test.tsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import { Layout } from "../layout/layout";
+import { BirdsForm } from "../components/BirdsForm";
+
+const TestPage = () => {
+  return (
+    <Fragment>
+      <Layout>
+        <h2>Four Birds Test</h2>
+        <BirdsForm />
+      </Layout>
+    </Fragment>
+  );
+};
+
+//Questions coming from database
+//Display four at once
+//All question patterns the same - four generic pages
