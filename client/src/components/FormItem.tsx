8544d2354da2058563eff29b82ee7552026a0da4
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-16 22:10:20 +1000

    formItem component start

---
+++ client/src/components/FormItem.tsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export const FormItem = ({ FormData, setForm, questionData }: any) => {
+  return (
+    <Fragment>
+      <h2>Test Name Right Now</h2>
+    </Fragment>
+  );
+};
