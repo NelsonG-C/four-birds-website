025acc9d79683a2385ce1dddb470243e450ac850
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-17 14:36:40 +1000

    basic load of a q from data

--- client/src/components/FormItem.tsx
+++ client/src/components/FormItem.tsx
@@ -1,19 +1,20 @@
 import React, { Fragment } from "react";
 import "../styles/styles.css";
 
-export const FormItem = ({ FormData, setForm, questionData }: any) => {
+export const FormItem = ({ FormData, setForm, questions }: any) => {
+  console.log("questions", questions);
+  let comp;
+  let val = 2;
   return (
-    <Fragment>
-      <h2>Test Name Right Now</h2>
-      <div className="inline-radio">
-        <p>Agree</p>
-        <input type="radio" id="" value="1" />
-        <input type="radio" id="" value="2" />
-        <input type="radio" id="" value="3" />
-        <input type="radio" id="" value="4" />
-        <input type="radio" id="" value="5" />
-        <p>Disagree</p>
-      </div>
-    </Fragment>
+    <div className="inline-radio">
+      <h2>{questions[0].q_text}</h2>
+      <p>Agree</p>
+      <input type="radio" id="" value="1" />
+      <input type="radio" id="" value="2" />
+      <input type="radio" id="" value="3" />
+      <input type="radio" id="" value="4" />
+      <input type="radio" id="" value="5" />
+      <p>Disagree</p>
+    </div>
   );
 };
