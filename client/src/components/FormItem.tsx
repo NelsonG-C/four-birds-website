3f04815f42d5ed0c262ab26fa46af26ee557f299
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-17 11:29:36 +1000

    inline radio button base add

--- client/src/components/FormItem.tsx
+++ client/src/components/FormItem.tsx
@@ -1,9 +1,19 @@
 import React, { Fragment } from "react";
+import "../styles/styles.css";
 
 export const FormItem = ({ FormData, setForm, questionData }: any) => {
   return (
     <Fragment>
       <h2>Test Name Right Now</h2>
+      <div className="inline-radio">
+        <p>Agree</p>
+        <input type="radio" id="" value="1" />
+        <input type="radio" id="" value="2" />
+        <input type="radio" id="" value="3" />
+        <input type="radio" id="" value="4" />
+        <input type="radio" id="" value="5" />
+        <p>Disagree</p>
+      </div>
     </Fragment>
   );
 };
