8343a90d8c98be2ff9a5f91763e7c2f113878ed8
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-17 16:42:36 +1000

    dynamic form content add

--- client/src/components/FormItem.tsx
+++ client/src/components/FormItem.tsx
@@ -1,13 +1,13 @@
 import React, { Fragment } from "react";
 import "../styles/styles.css";
 
-export const FormItem = ({ FormData, setForm, questions }: any) => {
+export const FormItem = ({ FormData, setForm, questions, i }: any) => {
   console.log("questions", questions);
   let comp;
   let val = 2;
   return (
     <div className="inline-radio">
-      <h2>{questions[0].q_text}</h2>
+      <h2>{questions[i].q_text}</h2>
       <p>Agree</p>
       <input type="radio" id="" value="1" />
       <input type="radio" id="" value="2" />
