02521b8babe0089632e22945f1ab85b91f6db7af
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-21 17:47:31 +1000

    fixed the data displayed for the result

--- client/src/pages/results.tsx
+++ client/src/pages/results.tsx
@@ -1,5 +1,31 @@
 import React, { Fragment, useEffect, useState } from "react";
+
 import { Row, Container } from "react-bootstrap";
+import eagle from "../logo.svg";
+import owl from "../logo.svg";
+import peacock from "../logo.svg";
+import dove from "../logo.svg";
+
+const Birds: any = [
+  {
+    D: {
+      name: "Eagle",
+      image: eagle,
+    },
+    I: {
+      name: "Peacock",
+      image: peacock,
+    },
+    S: {
+      name: "Dove",
+      image: dove,
+    },
+    C: {
+      name: "Owl",
+      image: owl,
+    },
+  },
+];
 
 export const ResultsPage = () => {
   const [results, setResult] = useState<any>([]);
@@ -29,14 +55,20 @@ export const ResultsPage = () => {
     console.log("In here");
     return <div></div>;
   } else {
-    console.log(results[0].top_result);
+    const letter: any = results[0].top_result;
+    console.log(letter);
+    const bird: any = Birds[0][letter];
+    console.log(bird);
     return (
       <Fragment>
         <h2>Results Time!</h2>
         <h3>Your main bird type is...</h3>
         <Container>
           <Row>
-            <h3>{results[0].top_result}</h3>
+            <h3>{bird.name}</h3>
+          </Row>
+          <Row>
+            <img src={bird.image} alt="" />
           </Row>
         </Container>
       </Fragment>
