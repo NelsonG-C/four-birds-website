5555ea389ae43e0d8d144a8309b0649a133814d7
Nelson Gardner-Challis <nelson@tfyc.com.au>
2020-09-23 21:58:28 +1000

    updated db schema

--- database.sql
+++ database.sql
@@ -1,22 +1,23 @@
-CREATE DATABASE fourbirds;
+-- CREATE DATABASE fourbirds;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE Results(
-    id uuid DEFAULT uuid_generate_v4 (),
+CREATE TABLE Questions(
+    id SERIAL PRIMARY KEY,
+    q_text TEXT,
+    q_type CHAR
+);
+
+CREATE TABLE answers(
+    id uuid DEFAULT uuid_generate_v4(),
     question_id SERIAL REFERENCES Questions(id),
     answer int,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Questions(
-    id SERIAL PRIMARY KEY,
-    q_text TEXT
-);
-
 CREATE TABLE Users(
     username TEXT,
     password TEXT,
-    user_id PRIMARY KEY,
+    user_id uuid PRIMARY KEY,
     top_result CHAR
 );
