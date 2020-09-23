e9b9cb7895b24c140a6d9daf982cebaf293aac4d
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-09-23 22:14:32 +1000

    added type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import { Layout } from "./layout/layout";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //PAGES
 import HomePage from "./pages/home";
